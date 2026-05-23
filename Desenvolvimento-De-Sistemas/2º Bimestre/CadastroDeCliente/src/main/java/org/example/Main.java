b417491b7779c268d3a75611c64347099fb2ce15
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-05-22 22:04:51 -0300

    Docker
    Um absurdo, muito bom, facil de usar tbm

--- Desenvolvimento-De-Sistemas/2º Bimestre/CadastroDeCliente/src/main/java/org/example/Main.java
+++ Desenvolvimento-De-Sistemas/2º Bimestre/CadastroDeCliente/src/main/java/org/example/Main.java
@@ -5,10 +5,10 @@ import javax.swing.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        CadastroCliente Client = new CadastroCliente();
-        Botoes Botoes = new Botoes();
-        Password Pass = new Password();
-        FormattedTextField FldPass = new FormattedTextField();
+//        CadastroCliente Client = new CadastroCliente();
+//        Botoes Botoes = new Botoes();
+//        Password Pass = new Password();
+//        FormattedTextField FldPass = new FormattedTextField();
         Calculadora calc = new Calculadora();
     }
 }
