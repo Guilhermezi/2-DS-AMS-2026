ac970033b8d21783b72842d74165baa8288892f3
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-08-27 17:44:00 -0300

    acho que agora o crud está certinho

--- Programacao-Web-II/3ºBimestre/primeiroCrud/edit.php
+++ Programacao-Web-II/3ºBimestre/primeiroCrud/edit.php
@@ -12,9 +12,12 @@
     $stmt->execute();
     $aluno = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    echo "<link rel='stylesheet' href='style.css'>";
     echo "<h1>Editar Aluno</h1>";
     echo "<form action='update.php' method='POST'>";
-    echo "<input type='hidden' name='id' value='" . htmlspecialchars($id) . "'>";
+    echo "<input type='hidden' name='id_original' value='" . htmlspecialchars($id) . "'>";
+    echo "<label for='id'>ID:</label>";
+    echo "<input type='number' id='id' name='id' value='" . htmlspecialchars($id) . "' required>";
     echo "<label for='nome'>Nome:</label>";
     echo "<input type='text' id='nome' name='nome' value='" . htmlspecialchars($aluno['nome'] ?? '') . "' required>";
     echo "<label for='curso'>Curso:</label>";
--- Programacao-Web-II/3ºBimestre/primeiroCrud/update.php
+++ Programacao-Web-II/3ºBimestre/primeiroCrud/update.php
@@ -1,15 +1,17 @@
 <?php
     include 'conexao.php';
 
+    $id_original = $_POST['id_original'];
     $id = $_POST['id'];
     $nome = $_POST['nome'];
     $curso = $_POST['curso'];
 
-    $sql = "UPDATE aluno SET nome = :nome, curso = :curso WHERE id = :id";
+    $sql = "UPDATE aluno SET id = :id, nome = :nome, curso = :curso WHERE id = :id_original";
     $stmt = $conexao->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->bindParam(':nome', $nome);
     $stmt->bindParam(':curso', $curso);
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':id_original', $id_original, PDO::PARAM_INT);
 
     if ($stmt->execute()) {
         echo "<link rel='stylesheet' href='style.css'>";
